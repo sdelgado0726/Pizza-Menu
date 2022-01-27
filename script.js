ca1cff73f1ea2b95d5f725f2850d531d2b483497
Stephanie Delgado <stephaniedelgado@Stephanies-MacBook-Air.local>
2022-01-27 17:39:59 -0500

    Pizza Order with for loop without if statement

---
+++ script.js
@@ -0,0 +1,71 @@
+// Global variables
+
+var crust = document.getElementById("crust");
+var sauce = document.getElementById("sauce");
+
+// toppings array
+
+var toppings = [];
+
+// event listener
+
+var formButton = document.getElementById("btn");
+formButton.addEventListener("click", function getName(event) {
+    event.preventDefault();
+
+    var topping1 = document.getElementById("topping1").value;
+    console.log(topping1);
+    var topping2 = document.getElementById("topping2").value;
+    console.log(topping2);
+    var topping3 = document.getElementById("topping3").value;
+    console.log(topping3);
+
+    toppings.push(topping1, topping2, topping3);
+
+
+    calculateTotal(toppings);
+
+})
+
+
+function calculateTotal(toppingArray) {  
+    let total = 0; 
+    let toppingCost = 2.50; 
+    let baseCost = 5.50; // cost of crust and sauce
+
+    // order string concatenation
+
+    let orderString = crust + " pizza with " + sauce;
+    
+    let toppingString = "Toppings: ";
+    
+
+    // For Loop
+
+    for (var i = 0; i < toppings.length; i++) {
+        total = total + toppingCost;
+        toppingString = toppingString + toppings[i] + " ";
+        
+        
+        }
+
+    // total = baseCost + cost of all toppings
+
+    total = total + baseCost;
+
+    // set DOM total += total
+    
+    document.getElementById("total$").innerHTML = "$" + total;
+    
+    // use DOM: = orderString
+    
+    document.getElementById("pizzaOrder").innerHTML = orderString;
+    
+    // use DOM: = toppingString
+    
+    document.getElementById("toppings").innerHTML = toppingString;
+}
+
+
+
+
